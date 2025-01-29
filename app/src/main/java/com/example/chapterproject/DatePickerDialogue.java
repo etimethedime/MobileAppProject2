fd9c4c86116fb1b77b72bddf9e03d42df58a2cdb
ezra5 <ezra54311@gmail.com>
2025-01-29 03:53:50 -0500

    calendar dialogue file created

---
+++ app/src/main/java/com/example/chapterproject/DatePickerDialogue.java
@@ -0,0 +1,19 @@
+package com.example.chapterproject;
+import android.os.Bundle;
+import android.widget.CalendarView;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ToggleButton;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.DialogFragment;
+
+import java.util.Calendar;
+
+public class DatePickerDialogue extends DialogFragment{
+    Calendar selectDate;
+}
