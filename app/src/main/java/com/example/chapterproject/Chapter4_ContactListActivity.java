82e4341d24ac88aab149189dc19df3cdf8956525
ezra5 <ezra54311@gmail.com>
2025-01-29 02:01:18 -0500

    3 new activities pt2

--- app/src/main/java/com/example/chapterproject/Chapter4_ContactListActivity.java
+++ app/src/main/java/com/example/chapterproject/Chapter4_ContactListActivity.java
@@ -8,13 +8,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class Chapter4_MyContactList extends AppCompatActivity {
+public class Chapter4_ContactListActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_chapter4_my_contact_list);
+        setContentView(R.layout.activity_chapter4_contact_list);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
