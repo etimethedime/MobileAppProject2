8aabcf16adad5c6873c37deb6581d1ce1a6e1cc3
ezra5 <ezra54311@gmail.com>
2025-01-29 03:31:13 -0500

    chapter 4 working

--- app/src/main/java/com/example/chapterproject/Chapter4_MyContactsActivity.java
+++ app/src/main/java/com/example/chapterproject/Chapter4_MyContactsActivity.java
@@ -1,6 +1,10 @@
 package com.example.chapterproject;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ToggleButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,7 +24,68 @@ public class Chapter4_MyContactsActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        initContactListButton();
+        initMapButton();
+        initSettingsButton();
+        initToggleButton();
+        setForEditing(false);
+
     }
 
+    private void initContactListButton() {
+        ImageButton contactListButton = findViewById(R.id.contactListButton);
+        contactListButton.setOnClickListener(v -> {
+            Intent listIntent = new Intent(Chapter4_MyContactsActivity.this, Chapter4_ContactListActivity.class);
+            listIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(listIntent);
+        });
+    }
+    private void initMapButton() {
+        ImageButton mapButton = findViewById(R.id.contactMapButton);
+        mapButton.setOnClickListener(v -> {
+            Intent listIntent = new Intent(Chapter4_MyContactsActivity.this, Chapter4_MapActivity.class);
+            listIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(listIntent);
+        });
+    }
+    private void initSettingsButton() {
+        ImageButton settingsButton = findViewById(R.id.contactSettingsButton);
+        settingsButton.setOnClickListener(v -> {
+            Intent listIntent = new Intent(Chapter4_MyContactsActivity.this, Chapter4_SettingsActivity.class);
+            listIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(listIntent);
+        });
+    }
 
+    private void initToggleButton() {
+        ToggleButton toggleButton = findViewById(R.id.onOffButton);
+        toggleButton.setOnClickListener(v -> {
+            setForEditing(toggleButton.isChecked());
+        });
+    }
+    private void setForEditing(boolean enabled) {
+       EditText editName = findViewById(R.id.nameText);
+       EditText editStreet = findViewById(R.id.streetText);
+       EditText editCity = findViewById(R.id.cityText);
+       EditText editState = findViewById(R.id.stateText);
+       EditText editZipcode = findViewById(R.id.zipcodeText);
+       EditText editCell = findViewById(R.id.cellNumberText);
+       EditText editHome = findViewById(R.id.homePhoneText);
+       EditText editEmail = findViewById(R.id.emailText);
+
+       editName.setEnabled(enabled);
+       editStreet.setEnabled(enabled);
+       editCity.setEnabled(enabled);
+       editState.setEnabled(enabled);
+       editZipcode.setEnabled(enabled);
+       editCell.setEnabled(enabled);
+       editHome.setEnabled(enabled);
+       editEmail.setEnabled(enabled);
+
+       if (enabled) {
+           editName.requestFocus();
+       }
+
+    }
 }
