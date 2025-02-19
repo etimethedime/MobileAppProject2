f1c676df61aa61129cc82ff61d7fe1527c382e56
ezra5 <ezra54311@gmail.com>
2025-02-18 23:55:44 -0500

    Contact List activity loads view

--- app/src/main/java/com/example/chapterproject/Chapter4_ContactListActivity.java
+++ app/src/main/java/com/example/chapterproject/Chapter4_ContactListActivity.java
@@ -1,12 +1,19 @@
 package com.example.chapterproject;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class Chapter4_ContactListActivity extends AppCompatActivity {
 
@@ -20,5 +27,47 @@ public class Chapter4_ContactListActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        initContactListButton();
+        initMapButton();
+        initSettingsButton();
+
+        ContactDataSource ds = new ContactDataSource(Chapter4_ContactListActivity.this);
+        ArrayList<String> names;
+
+        try {
+            ds.open();
+            names = ds.getContactNames();
+            ds.close();
+            RecyclerView contactList = findViewById(R.id.contactRV);
+            RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+            contactList.setLayoutManager(layoutManager);
+            ContactAdapter adapter = new ContactAdapter(names);
+            contactList.setAdapter(adapter);
+        }
+        catch (Exception e) {
+            Toast.makeText(this, "Error retrieving contacts", Toast.LENGTH_SHORT).show();
+        }
+
     }
+    private void initContactListButton() {
+        ImageButton settingsButton = findViewById(R.id.contactSettingsButton);
+        settingsButton.setEnabled(false);
+    }
+    private void initMapButton() {
+        ImageButton mapButton = findViewById(R.id.contactMapButton);
+        mapButton.setOnClickListener(v -> {
+            Intent listIntent = new Intent(Chapter4_ContactListActivity.this, Chapter4_MapActivity.class);
+            listIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(listIntent);
+        });
+    }
+    private void initSettingsButton() {
+        ImageButton settingsButton = findViewById(R.id.contactSettingsButton);
+        settingsButton.setOnClickListener(v -> {
+            Intent listIntent = new Intent(Chapter4_ContactListActivity.this, Chapter4_SettingsActivity.class);
+            listIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(listIntent);
+        });
+    }
+
 }
--- app/src/main/java/com/example/chapterproject/ContactAdapter.java
+++ app/src/main/java/com/example/chapterproject/ContactAdapter.java
@@ -1,24 +1,46 @@
 package com.example.chapterproject;
 
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
+
 public class ContactAdapter extends RecyclerView.Adapter {
+    private ArrayList<String> contactData;
+    public class ContactViewHolder extends RecyclerView.ViewHolder {
+        public TextView tvContactName;
+        public ContactViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tvContactName = itemView.findViewById(R.id.nameText);
+        }
+        public TextView getTvContactName() {
+            return tvContactName;
+        }
+    }
+    public ContactAdapter(ArrayList<String> arrayList){
+        contactData = arrayList;
+    }
+
     @NonNull
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.simple_item_view, parent, false);
+        return new ContactViewHolder(v);
     }
 
     @Override
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
-
+        ContactViewHolder viewHolder = (ContactViewHolder) holder;
+        viewHolder.getTvContactName().setText(contactData.get(position));
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return contactData.size();
     }
 }
